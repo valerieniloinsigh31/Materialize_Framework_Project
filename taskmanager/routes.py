70535bbf2e8a3b4d9a48ca829df00f4cb6e1b757
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-05-28 22:41:53 +0000

    Add categories and add-category templates and routes

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -7,3 +7,10 @@ from taskmanager.models import Category, Task
 def home():
     return render_template("tasks.html")
 
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
+
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    return render_template("add_category.html")
