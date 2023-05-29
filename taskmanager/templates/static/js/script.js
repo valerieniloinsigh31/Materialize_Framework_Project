8784b5c79aa212ebf3e10ffba003fb62357cbcf7
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-05-29 18:11:30 +0000

    Add 'add_task' template, app routes and relevant js extracts

--- taskmanager/templates/static/js/script.js
+++ taskmanager/templates/static/js/script.js
@@ -2,4 +2,16 @@ document.addEventListener('DOMContentLoaded', function() {
     //sidenav initialisation
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    //datepicker initialisation
+    let datepicker = document.querySelectorAll('datepicker');
+    M.Datepicker.init(datepicker {
+      format:"dd mm, yyyy",
+      i18n: {done: "Select"}
+    }
+      );
+
+     //select initialisation
+     let selects = document.querySelectorAll("select");
+     M.FormSelect.init(selects);
   });
