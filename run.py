2e7bd7a18225755d9c44254df8252fdea8b03c3b
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-05-31 08:59:31 +0000

    Readded add_task and updates as workspace had not saved these

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 import os
 from taskmanager import app
 
-if __name__== "__main__": #checking if name class is equal to default main string 
+
+if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
         debug=os.environ.get("DEBUG")
     )
+    
--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -2,16 +2,18 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
-    import env #noqa
+    import env   # noqa
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+if os.path.exists("env.py"):
+    import env  # noqa
 
-#create instance of Flask
+
 app = Flask(__name__)
-
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")  #DB_URL is database location to be set up later
-
-#ccreate an instance of imported SQLAlchemy class, which will be assigned to a variable of db and set to the instance of the Flask app:
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
 
-from taskmanager import routes #noqa
+from taskmanager import routes  # noqa
